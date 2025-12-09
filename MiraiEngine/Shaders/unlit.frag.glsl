d277ae5bbfd89313f4bc02a045709f84d384ab13
Itachi546 <28756796+Itachi546@users.noreply.github.com>
2025-12-09 22:17:27 +0545

    Add unlit shader

---
+++ MiraiEngine/Shaders/unlit.frag.glsl
@@ -0,0 +1,39 @@
+#version 460
+#extension GL_GOOGLE_include_directive : enable
+
+#include "utils/per-frame-data.glsl"
+#include "utils/bindless.glsl"
+
+layout(location = 0) out vec4 fragColor;
+
+layout(location = 0) in FS_IN {
+    vec2 uv;
+    flat uint mat_id;
+} fs_in;
+
+layout(set = 0, binding = 0) uniform PerFrameBinding {
+    PerFrameData per_frame_data;
+};
+
+struct UnlitMaterial {
+    vec4 color;
+
+    uint texture_id;
+    uint padding[3];
+};
+
+layout(set = 3, binding = 1) readonly buffer Materials {
+    UnlitMaterial materials[];
+};
+
+
+void main() {
+   UnlitMaterial material = materials[fs_in.mat_id];
+   vec3 col = material.color.rgb;
+
+    if (material.texture_id != K_INVALID_TEXTURE)
+        col *= sample_texture(material.texture_id, fs_in.uv).rgb;
+
+   fragColor = vec4(col, 1.0f);
+}
+
---
+++ MiraiEngine/Shaders/unlit.vert.glsl
@@ -0,0 +1,41 @@
+#version 460
+
+layout(location = 0) out VS_OUT {
+    vec3 normal;
+    vec2 uv;
+    flat uint mat_id;
+}
+vs_out;
+
+#extension GL_GOOGLE_include_directive : enable
+#extension GL_ARB_shader_draw_parameters : enable
+
+#include "utils/vertexdata.glsl"
+#include "utils/per-frame-data.glsl"
+
+layout(set = 0, binding = 0) uniform PerFrameBinding {
+    PerFrameData per_frame_data;
+};
+
+layout(set = 2, binding = 0) readonly buffer VertexData {
+    Vertex vertices[];
+};
+
+layout(set = 3, binding = 0) readonly buffer Transform {
+    mat4 transforms[];
+};
+
+layout(push_constant) uniform PushConstants {
+    uint transform_id;
+    uint material_id;
+    uint padding[2];
+};
+
+void main() {
+    Vertex vertex = vertices[gl_VertexIndex];
+    mat4 M = transforms[transform_id];
+
+    vec3 position = vec3(vertex.px, vertex.py, vertex.pz);
+    vec4 world_pos = M * vec4(position, 1.0f);
+    gl_Position = per_frame_data.VP * world_pos;
+}
